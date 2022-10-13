942d1821a952ed15a5e92896d77fe1834defb571
Your Name <you@example.com>
2022-10-13 19:43:14 +0300

    u

--- lib/screens/login&register/loginPage.dart
+++ lib/screens/login&register/loginPage.dart
@@ -1,3 +1,6 @@
+import 'package:case_store/components/my_button.dart';
+import 'package:case_store/components/textbox.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -8,8 +11,57 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Text("Welcome to our app"),
+              ),
+              Divider(
+                height: 25,
+              ),
+              Text("email"),
+              MyTextBox(
+                  textType: TextInputType.emailAddress,
+                  contlr: email,
+                  hintText: "Enter your email",
+                  scureText: false),
+              SizedBox(
+                height: 30,
+              ),
+              Text("password"),
+              MyTextBox(
+                  contlr: password,
+                  hintText: "Enter your password",
+                  scureText: true),
+              Divider(height: 25),
+              MyButton(
+                color: Colors.green[800]!,
+                title: "Login",
+                onPressed: () async {
+                  try {
+                    var authenticationobject = FirebaseAuth.instance;
+
+                    UserCredential myUser =
+                        await authenticationobject.signInWithEmailAndPassword(
+                            email: email.text, password: password.text);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("added successfully")));
+                  } catch (e) {
+                    ScaffoldMessenger.of(context)
+                        .showSnackBar(SnackBar(content: Text("$e")));
+                  }
+                },
+              ),
+            ]),
+      ),
+    );
   }
 }
